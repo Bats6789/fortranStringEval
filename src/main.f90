1116c45d3d6efb5c4c9a7d4c16ffe852eec57193
Blake Wingard <bwingard@ibml.com>
2024-09-19 14:31:37 -0500

    Added getopt support

--- src/main.f90
+++ src/main.f90
@@ -1,7 +1,20 @@
 program eval
-
+    use :: f90getopt
     implicit none
+    character(len=*), parameter :: VERSION = '0.1'
+    type(option_s) :: opts(1)
+    
+    opts(1) = option_s('version', .false., 'v')
 
-    print '(A)', 'Hello world!'
+    do
+        select case (getopt('a:v', opts))
+            case (char(0))
+                exit
+            case ('v')
+                print '(A, A3)', 'version ', VERSION
+                stop
+        end select
+    end do
 
+    print '(A)', 'Hello world'
 end program eval
